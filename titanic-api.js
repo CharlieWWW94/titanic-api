bfba7c7fea34b4dc6f3823f1c5582f6f7a240e0d
Charlie <cdwhiteside@protonmail.com>
2022-09-14 13:37:57 +0100

    Add search functionality by survivor

--- titanic-api.js
+++ titanic-api.js
@@ -1,67 +1,35 @@
 import express from "express";
-
+import td from "./titanic-data.json" assert { type: "json" };
 const app = express();
 const port = 3000;
 app.listen(port);
 
-//Data
+const findPassengers = (id) => {
+  return td.find((passenger) => passenger.PassengerId === parseInt(id));
+};
 
-const todos = [
-  {
-    id: 0,
-    title: "Todo 1",
-    desc: "This is my first todo",
-    completed: true,
-  },
-  {
-    id: 1,
-    title: "Todo 2",
-    desc: "This is my second todo",
-    completed: true,
-  },
-  {
-    id: 2,
-    title: "Todo 3",
-    desc: "This is my third todo",
-    completed: true,
-  },
-  {
-    id: 3,
-    title: "Todo 4",
-    desc: "This is my fourth todo",
-    completed: true,
-  },
-  {
-    id: 4,
-    title: "Todo 5",
-    desc: "This is my fifth todo",
-    completed: true,
-  },
-];
-
-app.use((req, res, next) => {
-  console.log(Date.now());
-  next();
-});
+const findSurvivors = (survStatus) => {
+  console.log(survStatus)
+  console.log(typeof survStatus)
+  return td.filter((passenger) => passenger.Survived === parseInt(survStatus));
+};
 
 app.get("/", (request, response) => {
-  response.status(200).json(todos);
+  response.status(200).json(td);
 });
 
-app.get("/todos", (request, response) => {
-  response.status(200).json(todos);
+app.get("/passengers", (request, response) => {
+  response.status(200).json(td);
 });
 
-// app.get("/todos/:id", (request, response) => {
-//     console.log(typeof request.params.id);
-//     response
-//     .status(200)
-//     .json({ data: todos.find((todo) => todo.id === parseInt(request.params.id)) });
-// });
-
-
-app.get("/todos/:id", (request, response) => {
-    response
-      .status(200)
-      .json({ data: todos.find((todo) => todo.id === parseInt(request.params.id)) });
+app.get("/passengers/id/:id", (request, response) => {
+  response.status(200).json({
+    data: findPassengers(request.params.id),
   });
+});
+
+app.get("/passengers/survived/:surv", (request, response) => {
+  console.log(request.params.surv)
+  console.log(typeof request.params.surv)
+  response.status(200).json({ data: findSurvivors(request.params.surv) });
+});
